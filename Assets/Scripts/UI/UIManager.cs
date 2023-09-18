7aeb117ce6c69533e2bcbf13bc9477644961ba7a
Aleksandar <aleksander.tanev.51@gmail.com>
2023-09-18 16:45:04 +0300

    add FPS display

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,7 +6,9 @@ using TMPro;
 public class UIManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _ballCountText;
+    [SerializeField] private TextMeshProUGUI _fpsText;
 
+    private float _deltaTime;
     private SpheresManager manager;
 
     private void Start()
@@ -17,5 +19,9 @@ public class UIManager : MonoBehaviour
     private void Update()
     {
         _ballCountText.text = manager.SpheresCount.ToString();
+
+        _deltaTime += (Time.deltaTime - _deltaTime) * 0.1f;
+        float fps = 1.0f / _deltaTime;
+        _fpsText.text = Mathf.Ceil(fps).ToString();
     }
 }
